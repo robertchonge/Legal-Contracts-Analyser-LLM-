cb9e12a72ebc39ef9f904940d3c798707e77a1ae
robertchonge <106912093+robertchonge@users.noreply.github.com>
2025-07-21 18:54:10 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,6 @@
+from django.db import models
+
+class UploadedContract(models.Model):
+    file = models.FileField(upload_to='contracts/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
 
