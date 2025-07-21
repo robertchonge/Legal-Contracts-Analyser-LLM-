469a64192a9142e1120b49679f07a56a232abaf8
robertchonge <106912093+robertchonge@users.noreply.github.com>
2025-07-21 18:55:26 +0200

    Update serializers.py

--- serializers.py
+++ serializers.py
@@ -1,1 +1,7 @@
+from rest_framework import serializers
+from .models import UploadedContract
+class UploadedContractSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UploadedContract
+        fields = ['id', 'file', 'uploaded_at']
 
